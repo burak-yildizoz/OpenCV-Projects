e03866d4c308e420a0afd8409fa79201a09d9aff
Burak <burak.yildizoz@boun.edu.tr>
2020-04-24 00:02:40 +0300

    Calibrate Fisheye create .gitignore

---
+++ Calibrate/Fisheye.cpp
@@ -0,0 +1,93 @@
+// OpenCV includes
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+// standard library
+#include <vector>
+#include <fstream>
+#include <string>
+// handy tools such as DEBUG, CHECK, etc.
+#include "general.hpp"
+// header file
+#include "Fisheye.hpp"
+
+Fisheye::Fisheye(int numBoards, int numCornersHor, int numCornersVer,
+                 const std::string &object_filename, const std::string &image_filename)
+    : Calibrate(numBoards, numCornersHor, numCornersVer, object_filename, image_filename) {}
+
+void Fisheye::calibrate_camera(const std::vector<std::vector<cv::Point3f>> &objectPoints,
+                               const std::vector<std::vector<cv::Point2f>> &imagePoints,
+                               cv::Size imageSize,
+                               cv::Mat &cameraMatrix,
+                               cv::Mat &distCoeffs)
+{
+    if (cameraMatrix.empty())
+    {
+        /*
+			[fx 0  cx
+			 0c fy cy
+			 0  0  1 ]
+		*/
+        cameraMatrix = cv::Mat(3, 3, CV_32FC1);
+    }
+    if (distCoeffs.empty())
+        distCoeffs = cv::Mat(4, 1, CV_32FC1);
+    std::vector<cv::Mat> rvecs, tvecs;
+    int flags = cv::fisheye::CALIB_RECOMPUTE_EXTRINSIC + cv::fisheye::CALIB_CHECK_COND + cv::fisheye::CALIB_FIX_SKEW;
+    cv::TermCriteria criteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 1e-6);
+    std::cout << "Calibrating!" << std::endl;
+    double rms = cv::fisheye::calibrate(objectPoints, imagePoints, imageSize, cameraMatrix, distCoeffs,
+                                        rvecs, tvecs, flags, criteria);
+    std::cout << "Calibration done! rms = " << rms << std::endl;
+}
+
+void Fisheye::display_undistorted(const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs,
+                                  cv::Mat &img, std::string winname)
+{
+    CHECK(!img.empty());
+    bool show_once = winname.empty();
+    if (show_once)
+        winname = "undistorted";
+    cv::Mat map1, map2;
+    cv::fisheye::initUndistortRectifyMap(cameraMatrix, distCoeffs, cv::Mat::eye(3, 3, CV_32FC1),
+                                         cameraMatrix, img.size(), CV_16SC2, map1, map2);
+    cv::Mat imageUndistorted;
+    cv::remap(img, imageUndistorted, map1, map2, cv::INTER_LINEAR, cv::BORDER_CONSTANT);
+    imshow(winname, imageUndistorted);
+    if (show_once)
+    {
+        char ch = cv::waitKey(0);
+        CHECK(ch != 27);
+        img = imageUndistorted.clone();
+    }
+}
+
+void Fisheye::display_undistorted(const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs,
+                                  cv::VideoCapture *capture, std::string winname)
+{
+    cv::VideoCapture &cap = *capture;
+    // make sure image stream is open
+    CHECK(cap.isOpened());
+    cv::Mat img;
+    cap >> img;
+    CHECK(!img.empty());
+    CHECK(!winname.empty());
+
+    // for display purposes
+    cv::namedWindow(winname);
+
+    char ch = 0;
+    while (ch != 27) // ESC is pressed
+    {
+        if (img.empty())
+        {
+            std::cout << "End of image stream!" << std::endl;
+            break;
+        }
+        display_undistorted(cameraMatrix, distCoeffs, img, winname);
+        ch = cv::waitKey(1);
+        cap >> img;
+    }
+    cv::destroyWindow(winname);
+}
---
+++ Calibrate/Fisheye.hpp
@@ -0,0 +1,55 @@
+#ifndef FISHEYE_HPP
+#define FISHEYE_HPP
+
+// base class
+#include "Calibrate.hpp"
+
+class Fisheye : public Calibrate
+{
+
+public:
+    /** @brief Class for fisheye camera calibration
+	@param numBoards number of chessboard images to be calibrated
+	@param numCornersHor number of corners along width
+	@param numCornersVer  number of corners along heigth
+	@param object_filename The name of the external file that object_points will be saved & loaded
+	@param image_filename The name of the external file that image_points will be saved & loaded
+	@note The parameters specified here cannot be altered later
+	*/
+    explicit Fisheye(int numBoards, int numCornersHor = 9, int numCornersVer = 6,
+                     const std::string &object_filename = "object_points.txt",
+                     const std::string &image_filename = "image_points.txt");
+
+    /** @brief Uses cv::calibrateCamera, but with more default arguments
+	@param object_points The physical position of the corners (in 3D space)
+	@param image_points The location of the corners on in the image (in 2 dimensions)
+	@param imageSize Size of the image
+	@param cameraMatrix 3x3 matrix that represents intrinsic parameters. An empty matrix is converted to have fx=fy=1
+	@param distCoeffs Distortion coefficients
+	*/
+    static void calibrate_camera(const std::vector<std::vector<cv::Point3f>> &objectPoints,
+                                 const std::vector<std::vector<cv::Point2f>> &imagePoints,
+                                 cv::Size imageSize,
+                                 cv::Mat &cameraMatrix,
+                                 cv::Mat &distCoeffs);
+
+    /** @brief Given camera calibration parameters, undistorts the image and displays the result
+	@param cameraMatrix 3x3 matrix that represents intrinsic parameters
+	@param distCoeffs Camera distortion coefficients
+	@param img Image to be undistorted
+	@param winname Name of the window that the result will be displayed. If it is empty, the window is held until user presses a key, and img becomes the resulting image. Else, waitKey needs to be called
+	@note Do not specify *winname* to wait until user presses a key
+	*/
+    static void display_undistorted(const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs,
+                                    cv::Mat &img, std::string winname = "");
+
+    /** @brief This is an overloaded function, provided for convenience. It differs from the above function only in what argument(s) it accepts.
+	@param cap The image stream that will be undistorted. If specified, it must be open
+	@param winname Name of the window that the result will be displayed.
+	*/
+    static void display_undistorted(const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs,
+                                    cv::VideoCapture *cap = new cv::VideoCapture(0),
+                                    std::string winname = "undistorted");
+};
+
+#endif // FISHEYE_HPP
